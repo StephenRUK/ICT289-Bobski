1489b54371d5146f5a7e8bbe1a29e96d738df47b
StephenRUK <stephen_aoe@hotmail.com>
2015-05-20 09:29:10 +0800

    Renamed Physics to PhysicsObject. Commented out methods for future refactoring.

---
+++ PhysicsObject.h
@@ -0,0 +1,30 @@
+#ifndef PHYSOBJ_H
+#define PHYSOBJ_H
+
+#include "Includes.h"
+
+/**
+* Physics Object
+* Method prefix: physObj
+**/
+
+/*
+* Physics object for attaching to a GameObject.
+* Keeps track of relevant physics values.
+*/
+typedef struct PhysicsObject {
+	float mass;				// Mass in kg
+	float gravityFactor;	// 0=no gravity, 1=standard gravity, etc.
+
+	vec3 position;
+	vec3 velocity;			// Directed velocity in m/s^2
+	//vec3 acceleration;		// Directed acceleration in m/s^2
+
+} PhysicsObject;
+
+//void physObjApplyGravity(PhysicsObject* physics);
+//void physObjApplyForce(PhysicsObject* physics, vec3 force);
+//bool physObjCheckForCollision(PhysicsObject* a, PhysicsObject* b);
+//void physObjResolveCollision(PhysicsObject* a, PhysicsObject* b);
+
+#endif
