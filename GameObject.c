712cd2fcc0bcf752a9ace8f7fc5f7c1ff51cfcec
StephenRUK <stephen_aoe@hotmail.com>
2015-05-20 10:58:46 +0800

    GameObject update physics - position and velocity

--- GameObject.c
+++ GameObject.c
@@ -1,6 +1,16 @@
 #include "GameObject.h"
 
 void gameObjUpdatePhysics(GameObject* obj, float dt) {
+	Transform* transform = &(obj->transform);
 	PhysicsObject* phys = &(obj->physics);
+
+
+	// 1 - Update position
+	vec3 deltaPos;
+	mathVector3MultiplyScalar(dt, phys->velocity, deltaPos);
+	mathVector3Add(deltaPos, transform->position, transform->position);
+
+	// 2 - Update velocity
 	physObjUpdateVelocity(phys, dt);
+
 }
