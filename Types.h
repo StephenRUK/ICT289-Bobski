f680a8295ee490d87f0727ee918d4d0238e29692
StephenRUK <stephen_aoe@hotmail.com>
2015-05-27 23:35:07 +0800

    Added vertex2 type to be used for UV (texture) coordinates.

--- Types.h
+++ Types.h
@@ -4,6 +4,7 @@
 typedef float vec2[2];
 typedef float vec3[3];
 
-typedef float vertex3[3];
+typedef float vertex2[2];		// Use for texture coordinates
+typedef float vertex3[3];		// Use for vertices, normals
 
 #endif
